c129e3289b7ac7c64c63c9c10c2554337f0bae19
3-24 <dev.youngseok@gmail.com>
2019-10-02 16:22:30 +0900

    Separate search function

---
+++ js/norae.js
@@ -0,0 +1,20 @@
+function search(){
+	var input = document.getElementById('searchInput').value;
+	var table = document.getElementById('songTable');
+	var tr = table.getElementsByTagName('tr');
+	
+	for (var i=0;i<tr.length;i++){
+		var td = tr[i].getElementsByTagName('td')[0];
+		if (td){
+			var txtValue = td.textContent || td.innerText;
+			txtValue = txtValue.replace(/\s+/g, '');
+			if ((txtValue.includes(input.replace(/\s+/g, '')))){
+				tr[i].style.display="";
+			}
+			else{
+				tr[i].style.display="none";
+			}
+		}
+	}
+
+}
